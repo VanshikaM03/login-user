f1af15065e59e85f158eb9597677b49e38e51c25
Vanshika <vanshikamittal2020@gmail.com>
2023-07-16 08:16:10 +0530

    black

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import AddUser from "./components/Users/AddUser";
-import UsersList from "./components/Users/UsersList";
-import { useState } from "react";
+// import AddUser from "./components/Users/AddUser";
+// import UsersList from "./components/Users/UsersList";
+// import { useState } from "react";
 function App() {
-  const [usersList, setUsersList] = useState([]);
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ];
-    });
-  };
+  // const [usersList, setUsersList] = useState([]);
+  // const addUserHandler = (uName, uAge) => {
+  //   setUsersList((prevUsersList) => {
+  //     return [
+  //       ...prevUsersList,
+  //       { name: uName, age: uAge, id: Math.random().toString() },
+  //     ];
+  //   });
+  // };
   return (
     <div>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
+      {/* <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} /> */}
     </div>
   );
 }
